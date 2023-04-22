541bd342bb15fc7f07d09d3667d0783b222850d3
Random people <98900217+justaguy6@users.noreply.github.com>
2023-04-22 11:26:53 -0300

    Update WarningState.hx

--- source/WarningState.hx
+++ source/WarningState.hx
@@ -138,7 +138,7 @@ class WarningState extends MusicBeatState
    {
 	   
 	   var video:MP4Handler = new MP4Handler();
-	   video.playVideo(Asset2File.getPath(Paths.video(filename)));
+	   video.playVideo(Asset2File.getPath(Paths.video(name)));
 	   video.finishCallback = function()
 	   {
 			PlayState.storyDifficulty = 2;
